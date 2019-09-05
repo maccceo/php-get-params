50c41738a7b014f01b42247b27c54415e07d91f9
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-09-05 16:54:02 +0200

    Init

---
+++ esercizio.php
@@ -0,0 +1,19 @@
+ <!-- creare una pagina che legga in ingresso 3 parametri numerici (in GET) che rappresentano le 3 dimensioni di un parallelepipedo: lunghezza, larghezza, altezza; stampare le 3 dimensioni e calcolare il volume della figura (lun x lar x alt) -->
+
+<!DOCTYPE html>
+<html lang="" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Parallelepipedo</title>
+    <style>
+    </style>
+    <?php
+    
+    ?>
+  </head>
+  <body>
+  	<div id="main-container">
+
+  	</div>
+  </body>
+</html>
